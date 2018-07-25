1524ad079bda94797bf8ac52524f86eab5322b03
Ardhi Lukianto <ardhi@lukianto.com>
2018-07-25 09:43:07 +0700

    reg update

--- cuks/core/helper/url.js
+++ cuks/core/helper/url.js
@@ -4,7 +4,9 @@ module.exports = function(cuk) {
   const { _, helper } = cuk.pkg.core.lib
 
   return (name, params, opts) => {
-    const [pkgId, routeName, pkg] = helper('core:pkgSplitToken')(name, 'Invalid rest route name (%s)')
+    if (name.substr(0, 1) === '/' || name.substr(0, 7) === 'http://' || name.substr(0, 8) === 'https://')
+      return name
+    const [pkgId, routeName, pkg] = helper('core:pkgTokenSplit')(name, 'Invalid rest route name (%s)')
     const router = cuk.pkg.rest.lib.router
     return router.url(routeName, params, opts)
   }
