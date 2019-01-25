2933f409cb9ebc007c7d3901da2c6080ce1bf035
Ardhi Lukianto <ardhi@lukianto.com>
2019-01-25 11:17:42 +0700

    reg update

--- cuks/core/helper/convert_id_column.js
+++ cuks/core/helper/convert_id_column.js
@@ -5,14 +5,15 @@ module.exports = function (cuk) {
 
   return (name, src) => {
     if (_.isEmpty(src)) return src
-    let idCol = '_id'
-    if (name) idCol = helper('model:getIdColumn')(name)
+    let idCol = name
+    if (name && name.indexOf(':') > -1) idCol = helper('model:getIdColumn')(name)
+    if (_.isEmpty(idCol)) return src
     const idWanted = helper('core:config')('rest', 'key.id', '')
     if (idCol === idWanted) return src
     const isArr = _.isArray(src)
     const data = isArr ? _.cloneDeep(src) : [_.cloneDeep(src)]
     _.each(data, (d, i) => {
-      data[i][idWanted] = d[idCol]
+      data[i][idWanted] = d[idCol] || d['id']
       delete data[i][idCol]
     })
     return isArr ? data : data[0]
